aaf9fac7e495cdf4d89a88d45bca5113a5e62c33
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2024-02-17 12:43:13 +0100

    added admin fields

--- channel/admin.py
+++ channel/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from channel.models import Channel, ChannelMessage, ChannelThread, ChannelThreadMessage
 
-# Register your models here.
+
+admin.site.register(Channel)
+admin.site.register(ChannelMessage)
+admin.site.register(ChannelThread)
+admin.site.register(ChannelThreadMessage)
--- direct_message/admin.py
+++ direct_message/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from direct_message.models import DirectChat, DirectMessages, DirectChatThread, DirectChatThreadMessage 
 
-# Register your models here.
+
+admin.site.register(DirectChat)
+admin.site.register(DirectMessages)
+admin.site.register(DirectChatThread)
+admin.site.register(DirectChatThreadMessage)
