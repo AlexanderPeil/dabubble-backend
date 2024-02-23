03fef4964922ec63d6bd89dfa5e65942f9d5c66f
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2024-02-23 19:27:20 +0100

    created logic to update channels

--- channel/serializers.py
+++ channel/serializers.py
@@ -6,20 +6,22 @@ User = get_user_model()
 
 
 
+from rest_framework import serializers
+
 class ChannelSerializer(serializers.ModelSerializer):
-    created_by = serializers.ReadOnlyField(source='created_by.username')
+    created_by_full_name = serializers.SerializerMethodField()  # Änderung hier
     member_ids = serializers.ListField(child=serializers.IntegerField(), write_only=True, required=False)
 
     class Meta:
         model = Channel
-        fields = ['id', 'created_at', 'title', 'description', 'members', 'created_by', 'member_ids']
+        fields = ['id', 'created_at', 'title', 'description', 'members', 'created_by', 'member_ids', 'created_by_full_name']  # Füge created_by_full_name hinzu
         read_only_fields = ['id', 'created_at', 'members', 'created_by']
+
+    def get_created_by_full_name(self, obj):  # Stelle sicher, dass der Methodenname mit get_<Feldname> beginnt
+        return f"{obj.created_by.first_name} {obj.created_by.last_name}"
 
     def create(self, validated_data):
         member_ids = validated_data.pop('member_ids', [])
-        # creator = self.context['request'].user
-        # if creator.id not in member_ids:
-        #     member_ids.append(creator.id)
         channel = Channel.objects.create(**validated_data)
         channel.members.set(User.objects.filter(id__in=member_ids))
         return channel
